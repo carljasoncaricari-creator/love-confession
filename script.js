c484370b7c068a65a37538592f357a578d1d34ec
carljasoncaricari-creator <carljasoncaricari@gmail.com>
2025-09-17 20:31:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const proceedBtn = document.querySelector(".proceed");
   const resetBtn = document.querySelector(".reset");
+  const confessionBox = document.getElementById("confession");
 
   if (proceedBtn) {
     proceedBtn.addEventListener("click", () => {
-      alert("Hi Boss, kumusta ka? 😊 I know you're not doing good sometimes but laban lang ara grind paras future. I just want to say nga I'm still stalking your reposts and one really stood out to me — the phrase: 'you know a boy's real intentions when you reject them.' Can I ask, did you reject me ba? If yes then I guess this is my first time getting rejected HAHAHAHAHA. I saw all your reports about your past. Pero wakoy paki 💖");
+      confessionBox.style.display = "block";
+      proceedBtn.style.display = "none"; // hide proceed after click
     });
   }
 
